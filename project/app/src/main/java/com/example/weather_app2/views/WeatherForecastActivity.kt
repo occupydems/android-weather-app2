c16c340860bdfd3679267053e1a3c76c7b3cae48
Mee <mikepeskoff3@gmail.com>
2026-02-24 18:59:14 -0400

    Revert sticky headers to stickyHeaderCount=1 (matching build-20260224-223103)

--- project/app/src/main/java/com/example/weather_app2/views/WeatherForecastActivity.kt
+++ project/app/src/main/java/com/example/weather_app2/views/WeatherForecastActivity.kt
@@ -103,9 +103,9 @@ class WeatherForecastActivity : AppCompatActivity(), EasyPermissions.PermissionC
         }
         viewModel.errorStatus.observe(this,errorStatusObserver)
 
-        binding.hourlyForecastContainer.stickyHeaderCount = 2
-        binding.dailyForecastCard.stickyHeaderCount = 2
-        binding.adCard.stickyHeaderCount = 2
+        binding.hourlyForecastContainer.stickyHeaderCount = 1
+        binding.dailyForecastCard.stickyHeaderCount = 1
+        binding.adCard.stickyHeaderCount = 1
         stickyCards.add(binding.hourlyForecastContainer)
         stickyCards.add(binding.dailyForecastCard)
         stickyCards.add(binding.adCard)
